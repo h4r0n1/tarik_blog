17bb90c90a62e4930da0375b35842b9b961f8ba8
h4r0n1 <mdl.diop97@gmail.com>
2023-05-23 10:32:40 +0000

    23052023_ Update: Changed basic layout to incoporate my portfolio

---
+++ data/authors/mountaga.md
@@ -0,0 +1,12 @@
+---
+name: Mountaga Diao Leye Diop
+avatar: /static/images/avatar1.JPG
+occupation: FullStack Web Dev
+# company: Stanford University
+email: address@yoursite.com
+twitter: https://twitter.com/h4r0n1
+linkedin: https://www.linkedin.com/in/mdl-diop
+github: https://github.com/h4r0n1
+---
+
+Enthusiastic student recently graduated from the Virtual University of Senegal in Linguistics and currently learning Web development with some basic skills and the ability to work with a minimal supervision. Eager to bring value to your establishment through hard work and commitment to quality.
--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,13 +1,13 @@
-import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
+import SocialIcon from '@/components/social-icons'
 
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
 
   return (
     <>
-      <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
+      <PageSEO title={`À Propos - ${name}`} description={`À Propos de moi - ${name}`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
@@ -19,13 +19,13 @@ export default function AuthorLayout({ children, frontMatter }) {
             <Image
               src={avatar}
               alt="avatar"
-              width="192px"
-              height="192px"
-              className="h-48 w-48 rounded-full"
+              width="200px"
+              height="200px"
+              className="h-48 w-48 rounded-full object-cover"
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            {/* <div className="text-gray-500 dark:text-gray-400">{company}</div> */}
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
